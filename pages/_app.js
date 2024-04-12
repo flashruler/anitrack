51ce4240da7272ed98bc364b27ba6603ce4b7621
flashruler <44584657+flashruler@users.noreply.github.com>
2024-04-11 17:04:01 -0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import { AuthContextProvider } from '/context/AuthContext'
+
+function MyApp({ Component, pageProps }) {
+  return( 
+    < AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRouter } from 'next/navigation'
+
+function Home(){
+<div className="bg-slate-700">
+    <h1>test</h1>
+</div>
+}
+export default Home;
