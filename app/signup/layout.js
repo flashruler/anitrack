c18c594d82fc077ed74a49ddc3c6a74c6f28f054
flashruler <44584657+flashruler@users.noreply.github.com>
2024-08-02 14:56:30 -0700

    Create layout.js

---
+++ app/signup/layout.js
@@ -0,0 +1,18 @@
+"use client";
+import "../../styles/globals.css";
+import { AuthContextProvider } from "/context/AuthContext";
+export default function Layout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-200">
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
+    </html>
+  );
+}
